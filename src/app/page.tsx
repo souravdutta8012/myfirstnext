8e52c2170e8976d4d0ccf5c5516b0cc16b6392bc
souravdutta8012 <souravdutta2108@gmail.com>
2023-07-05 23:00:30 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-  return (
-    <>
-      <div className="bg-red-600">
-        Land
-      </div>
-    </>
-  )
+  redirect('/home');
 }
